9a183a9155a0f1bcfca2fe0a5b5073bc2c2fb300
Matias Escobar <mescobar@chicagobooth.edu>
2023-04-13 15:41:16 +0000

    add form working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
 
+  def addition_form
+    render({ :template => "calculation/add_form.html.erb" })
+  end
+
+  def addition_results
+    
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+
+    @result_add = @first_num + @second_num
+
+    render({ :template => "calculation/add_results.html.erb" })
+    
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
 
+  # add /wizard_add?first_num=234&second_num=234 get("/")
+
+  get("/add", { :controller => "application", :action => "addition_form" })
+  get("/wizard_add", { :controller => "application", :action => "addition_results" })
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
